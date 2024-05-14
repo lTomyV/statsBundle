4c4faeffcf2ab3d8e2b8a76e8a951932aed6782c
Tomy <50710890+lTomyV@users.noreply.github.com>
2024-05-14 01:33:03 -0300

    feat(updated configs)

--- config.lua
+++ config.lua
@@ -1,11 +1,21 @@
 Config = {}
 
+
+Config.consoleDebug = false
+Config.discordWebhook = "https://discord.com/api/webhooks/1136813686770630697/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+Config.testingCommands = false
 --[[
     Lista de comandos:
     - /add_k => Añade una kill al jugador que lo ejecuta
     - /add_d => Añade una muerte al jugador que lo ejecuta
     - /add_h => Añade una hora de juego al jugador que lo ejecuta
 ]]
-Config.testingCommands = false
 
-Config.discordWebhook = "https://discord.com/api/webhooks/1136813686770630697/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+--================================================================================================
+--================================================================================================
+--======================================== LEVELING SYSTEM =======================================
+--================================================================================================
+--================================================================================================
+Config.levelOneXP = 100
+Config.xpMultiplierToNextLevel = 2 -- If level 1 is 100xp, level 2 will be 200xp, level 3 will be 400xp, etc.
