a76ee659629edd73b39b508cc2b6f605e1f7bff7
Tomy <50710890+lTomyV@users.noreply.github.com>
2024-05-14 01:32:21 -0300

    feat(levelingsystem): Agregado sistema de niveles

--- SQL/query.sql
+++ SQL/query.sql
@@ -2,3 +2,4 @@ ALTER TABLE `players`
 	ADD COLUMN `play_time` INT NULL DEFAULT '0',
 	ADD COLUMN `char_kills` INT NULL DEFAULT '0',
 	ADD COLUMN `char_deaths` INT NULL DEFAULT '0';
+	ADD COLUMN `char_xp` INT NULL DEFAULT '0';
---
+++ server/levelingSystem.lua
@@ -0,0 +1,108 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+
+--================================================================================================
+--======================================= ADD XP =================================================
+--================================================================================================
+RegisterServerEvent('statsBundle:addXp')
+AddEventHandler('statsBundle:addXp', function(xp, token)
+    if validateToken(token) then
+        local PlayerId = QBCore.Functions.GetPlayer(source).PlayerData.citizenid
+        addXp(PlayerId, xp)
+    else
+        sendToDiscord(16753920, "XP LOG", "some one tried to exploit statsBundle:addXp")
+    end
+end)
+
+-- Column is char_xp in table players
+function addXp(PlayerId, xp)
+    MySQL.update(
+        'UPDATE players set char_xp = char_xp + @xp where citizenid = @citizenid',
+        {['@citizenid'] = PlayerId, ['@xp'] = xp},
+        function(affectedRows)
+            if affectedRows < 1 then
+                sendToDiscord(16753920, "XP LOG", "ERROR: No se pudo agregar " .. xp .. " xp a " .. PlayerId)
+            else
+                sendToDiscord(16753920, "XP LOG", PlayerId .. " ganó " .. xp .. " xp")
+            end
+        end
+    )
+end
+
+
+--================================================================================================
+--========================================= REMOVE XP ============================================
+--================================================================================================
+RegisterServerEvent('statsBundle:removeXp')
+AddEventHandler('statsBundle:removeXp', function(xp, token)
+    if validateToken(token) then
+        local PlayerId = QBCore.Functions.GetPlayer(source).PlayerData.citizenid
+        removeXp(PlayerId, xp)
+    else
+        sendToDiscord(16753920, "XP LOG", "some one tried to exploit statsBundle:removeXp")
+    end
+end)
+
+-- Column is char_xp in table players
+function removeXp(PlayerId, xp)
+    MySQL.update(
+        'UPDATE players set char_xp = char_xp - @xp where citizenid = @citizenid',
+        {['@citizenid'] = PlayerId, ['@xp'] = xp},
+        function(affectedRows)
+            if affectedRows < 1 then
+                sendToDiscord(16753920, "XP LOG", "ERROR: No se pudo quitar " .. xp .. " xp a " .. PlayerId)
+            else
+                sendToDiscord(16753920, "XP LOG", PlayerId .. " perdió " .. xp .. " xp")
+            end
+        end
+    )
+end
+
+
+-- Callbacks
+
+--================================================================================================
+--========================================= GET XP ===============================================
+--================================================================================================
+QBCore.Functions.CreateCallback('statsBundle:GetLevel', function(source, cb)
+    local PlayerId = QBCore.Functions.GetPlayer(source).PlayerData.citizenid
+    local xp = getXp(PlayerId)
+
+    if xp ~= nil then
+        local level, progress = calculateLevelAndProgress(xp)
+        local readyCB = {xp = xp, level = level, progress = progress}
+        cb(readyCB)
+    else
+        -- Notify the source using qbnotify
+        QBCore.Functions.Notify("No se pudo obtener el nivel", "error")
+    end
+end)
+
+function getXp(PlayerId, cb)
+    MySQL.Async.fetchAll(
+        'SELECT char_xp FROM players WHERE citizenid = @citizenid',
+        {['@citizenid'] = PlayerId},
+        function(result)
+            if result[1] ~= nil then
+                cb(result[1].char_xp)
+            else
+                cb(nil)
+            end
+        end
+    )
+end
+
+function calculateLevelAndProgress(currentXP)
+    local level = 1
+    local xpForNextLevel = Config.levelOneXP
+
+    while currentXP >= xpForNextLevel do
+        currentXP = currentXP - xpForNextLevel
+        level = level + 1
+        xpForNextLevel = xpForNextLevel * Config.xpMultiplierToNextLevel
+    end
+
+    local progress = (currentXP / xpForNextLevel) * 100
+
+    return level, progress
+end
